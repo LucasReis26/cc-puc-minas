f44597ae30055254cb9b3a716143f914f76c01f8
Lucas Reis <luctreis@gmail.com>
2025-02-18 16:46:51 -0300

    AEDs-II | TP-I - Ex01 && Ex02 Concluídos

--- terceiroPeriodo/aeds-ii/tps/tp-i/ex01/Palindromo.java
+++ terceiroPeriodo/aeds-ii/tps/tp-i/ex01/Palindromo.java
@@ -1,8 +1,35 @@
-package ex01;
+// package ex01;
 
 import java.util.Scanner;
 
 public class Palindromo{
+
+	static Integer strcmp(String word1, String word2){
+
+		int len1 = word1.length(),
+			len2 = word2.length(),
+			end = (len1 > len2) ? len2 : len1,
+			resp = 0;
+
+		for(int i = 0; i < end; i++){
+			if(word1.charAt(i) > word2.charAt(i)){
+				resp = 1;
+				i = end;
+			}else if(word1.charAt(i) < word2.charAt(i)){
+				resp = -1;
+				i = end;
+			}
+		}
+
+		if(resp == 0 && len1 != len2){
+			if(len1 > len2)
+				resp = 1;
+			else
+				resp = -1;
+		}
+
+		return resp;
+	}
 
     static Boolean ehPalindromo(String word){
 
@@ -24,11 +51,15 @@ public class Palindromo{
 
         String word = sc.nextLine();
 
-        if(ehPalindromo(word)){
-            System.out.println("SIM");
-        }else{
-            System.out.println("NÃO");
-        }
+		while(strcmp(word,"FIM") != 0){
+			if(ehPalindromo(word)){
+				System.out.println("SIM");
+			}else{
+				System.out.println("NAO");
+			}
+			word = sc.nextLine();
+		}
+
 
         sc.close();        
     }

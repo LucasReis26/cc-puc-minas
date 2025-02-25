a7645ade422e6c895fb62837e96b952c33e80845
Lucas Reis <luctreis@gmail.com>
2025-02-25 13:14:44 -0300

    AEDs-II | TP-I EX06 CONCLUIDO EX05 EM ANDAMENTO

--- terceiroPeriodo/aeds-ii/tps/tp-i/ex05/Algebra.java
+++ terceiroPeriodo/aeds-ii/tps/tp-i/ex05/Algebra.java
@@ -3,11 +3,49 @@ package ex05;
 import java.util.Scanner;
 
 public class Algebra{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        
 
-        sc.close();
-    }
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		Integer entryNum = sc.nextInt();
+
+		while(entryNum != 0){
+
+			Integer[] binaryValues = new Integer[entryNum];
+
+			for(int i = 0; i < entryNum; i++){
+
+				binaryValues[i] = sc.nextInt();
+
+			}
+			sc.nextLine();
+
+			String bExpression = sc.nextLine();
+
+			Integer countExpression = 0;
+
+			Integer belength = bExpression.length();
+
+			for(int i = 0; i < belength; i++)
+				if(bExpression.charAt(i) == '(')
+					countExpression++;
+			
+			char[] cbExp = bExpression.toCharArray();
+
+			for(int i = 0; i < belength; i++){
+				if(cbExp[i] >= 65 && cbExp[i] <= 90){
+					int charValue = cbExp[i];
+					cbExp[i] = (char)('0' + binaryValues[charValue - 65]);
+				}
+			}
+
+			bExpression = new String(cbExp);
+
+			System.out.println("há " + countExpression + " partes nessa expressão");
+			System.out.println(bExpression);
+		}
+
+		sc.close();
+	}
 }
--- terceiroPeriodo/aeds-ii/tps/tp-i/ex06/Is.java
+++ terceiroPeriodo/aeds-ii/tps/tp-i/ex06/Is.java
@@ -1,108 +1,137 @@
-package ex06;
+// package ex06;
 
 import java.util.Scanner;
 
 public class Is{
-    //Método para verificar se a String é um número real
-    static boolean isReal(String word) {
-        boolean resp = false;
-        Integer decimalPoint = 0;
-        for(int i = 0; i < word.length(); i++){
-            if(word.charAt(i) >= '0' && word.charAt(i) <= '9' || word.charAt(i) == '.' || word.charAt(i) == ','){
-                if(word.charAt(i) == '.' || word.charAt(i) == ',')
-                    decimalPoint++;
-                resp = true;
-            }
-            else{
-                i = word.length();
-                resp = false;
-            }
-        }
-        if(resp && decimalPoint == 1)
-            resp = true;
-        else
-            resp = false;
-        return resp;
-    }
-    //Método para verificar se a String é um número inteiro
-    static boolean isInteiro(String word){
-        boolean resp = false;
-        for(int i = 0; i < word.length(); i++){
-            if(word.charAt(i) >= '0' && word.charAt(i) <= '9')
-                resp = true;
-            else{
-                i = word.length();
-                resp = false;
-            }
-        }
-        return resp;
-    }
-    //Método para verificar se a String é composta somente por consoantes
-    static boolean isConsoante(String word){
-        char[] vogais = {'a','e','i','o','u'};
-        boolean resp = false;
+	//Verificador de String
+	public static Integer strcmp(String word1, String word2){
+		Integer wlength1 = word1.length(),
+				wlength2 = word2.length(),
+				minorLength = (wlength1 > wlength2) ? wlength2 : wlength1,
+				resp = 0;
 
-        for(int i = 0; i < word.length(); i++){
-            for(int j = 0; j < vogais.length; j++){
-                if(Character.toLowerCase(word.charAt(i)) != vogais[j] && (Character.toLowerCase(word.charAt(i)) >= 'a' && Character.toLowerCase(word.charAt(i)) <= 'z')){
-                    resp = true;
-                }else{
-                    j = vogais.length;
-                    resp = false;
-                } 
-            }
-            if(resp == false){
-                i = word.length();
-            } 
-        }
+		for(int i = 0; i < minorLength; i++){
+			if(word1.charAt(i) > word2.charAt(i)){
+				resp = 1;
+				i = minorLength;
+			}else if(word1.charAt(i) < word2.charAt(i)){
+				resp = -1;
+				i = minorLength;
+			}
+		}
 
-        return resp;
-    }
-    //Método para verificar se a String é composta somente por vogais
-    static boolean isVogal(String word){
-        char[] vogais = {'a','e','i','o','u'};
-        boolean resp = false;
+		if(resp == 0 && wlength1 != wlength2){
+			if(wlength1 > wlength2)
+				resp = 1;
+			else
+				resp = -1;
+		}
 
-        for(int i = 0; i < word.length(); i++){
-            for(int j = 0; j < vogais.length; j++){
-                if(Character.toLowerCase(word.charAt(i)) == vogais[j]){
-                    j = vogais.length;
-                    resp = true;
-                }else{
-                    resp = false;
-                } 
-            }
-            if(resp == false){
-                i = word.length();
-            } 
-        }
+		return resp;
+	}
+	//Método para verificar se a String é um número real
+	static boolean isReal(String word) {
+		boolean resp = false;
+		Integer decimalPoint = 0;
+		for(int i = 0; i < word.length(); i++){
+			if(word.charAt(i) >= '0' && word.charAt(i) <= '9' || word.charAt(i) == '.' || word.charAt(i) == ','){
+				if(word.charAt(i) == '.' || word.charAt(i) == ',')
+					decimalPoint++;
+				resp = true;
+			}
+			else{
+				i = word.length();
+				resp = false;
+			}
+		}
+		if(resp && decimalPoint == 1)
+			resp = true;
+		else
+			resp = false;
+		return resp;
+	}
+	//Método para verificar se a String é um número inteiro
+	static boolean isInteiro(String word){
+		boolean resp = false;
+		for(int i = 0; i < word.length(); i++){
+			if(word.charAt(i) >= '0' && word.charAt(i) <= '9')
+				resp = true;
+			else{
+				i = word.length();
+				resp = false;
+			}
+		}
+		return resp;
+	}
+	//Método para verificar se a String é composta somente por consoantes
+	static boolean isConsoante(String word){
+		char[] vogais = {'a','e','i','o','u'};
+		boolean resp = false;
 
-        return resp;
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine();
-        if(isVogal(word)){
-            System.out.print("SIM;");
-        }else{
-            System.out.print("NÃO;");
-        }
-        if(isConsoante(word)){
-            System.out.print(" SIM;");
-        }else{
-            System.out.print(" NÃO;");
-        }
-        if(isInteiro(word)){
-            System.out.print(" SIM;");
-        }else{
-            System.out.print(" NÃO;");
-        }
-        if(isReal(word)){
-            System.out.println(" SIM;");
-        }else{
-            System.out.println(" NÃO;");
-        }
+		for(int i = 0; i < word.length(); i++){
+			for(int j = 0; j < vogais.length; j++){
+				if(Character.toLowerCase(word.charAt(i)) != vogais[j] && (Character.toLowerCase(word.charAt(i)) >= 'a' && Character.toLowerCase(word.charAt(i)) <= 'z')){
+					resp = true;
+				}else{
+					j = vogais.length;
+					resp = false;
+				} 
+			}
+			if(resp == false){
+				i = word.length();
+			} 
+		}
 
-        sc.close();
-    }
+		return resp;
+	}
+	//Método para verificar se a String é composta somente por vogais
+	static boolean isVogal(String word){
+		char[] vogais = {'a','e','i','o','u'};
+		boolean resp = false;
+
+		for(int i = 0; i < word.length(); i++){
+			for(int j = 0; j < vogais.length; j++){
+				if(Character.toLowerCase(word.charAt(i)) == vogais[j]){
+					j = vogais.length;
+					resp = true;
+				}else{
+					resp = false;
+				} 
+			}
+			if(resp == false){
+				i = word.length();
+			} 
+		}
+
+		return resp;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		while(strcmp(word,"FIM") != 0){
+			if(isVogal(word)){
+				System.out.print("SIM");
+			}else{
+				System.out.print("NAO");
+			}
+			if(isConsoante(word)){
+				System.out.print(" SIM");
+			}else{
+				System.out.print(" NAO");
+			}
+			if(isInteiro(word)){
+				System.out.print(" SIM");
+			}else{
+				System.out.print(" NAO");
+			}
+			if(isReal(word)){
+				System.out.println(" SIM");
+			}else{
+				System.out.println(" NAO");
+			}
+			word = sc.nextLine();
+		}
+
+		sc.close();
+	}
 }

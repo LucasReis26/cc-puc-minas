63184e7d6b37aed2881c17a791166ee85fbdbf7f
Lucas Reis <luctreis@gmail.com>
2025-03-10 19:52:25 -0300

    last last updates

---
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1023/ex1023.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculaMedia(int mor[], int cons[], int med[], int mQuant[], int iQuant){
+
+	for(int i = 0; i < iQuant); i++)
+		mQuant[i] = 0;
+
+	for(int i = 0; i < iQuant; i++){
+		int mediaImovel = mor[i] / cons[i];
+		for(int j = 0; j < iQuant; j++){
+			if(mediaImovel == med[i])
+				
+		}
+	}
+}
+
+int main(){
+
+	int iQuant = -1,
+		cidade = 1;
+	
+	while(iQuant != 0){
+		scanf("%d",&iQuant);
+
+		int moradores[iQuant],
+			consume[iQuant],
+			mQuant[iQuant];
+
+		float media[iQuant]; 
+
+
+		for(int i = 0; i < iQuant; i++){
+			scanf("%d %d",&moradores[i], &consumo[i]);
+		}
+	}
+}

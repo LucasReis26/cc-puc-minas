f9fa28bbd2f2b252b656babfdc89d51beb5c00cf
Lucas Reis <luctreis@gmail.com>
2025-03-20 15:07:37 -0300

    Organizando melhor meu shellscript

--- terceiroPeriodo/aeds-ii/beecrowdTraining/.bin/dir.sh
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/.bin/dir.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
-echo "Digite a linguagem (c) Language ou (j)ava:";
-read y;
-echo "Digite o número da questão:";
-read x;
+writePubsLt10(){
+	nvim ex0$x/pubs/pub.in;
+	nvim ex0$x/pubs/pub.out;
+}
 
-if [ "$y" == "c" ]; then
+writePubsGe10(){
+	nvim ex$x/pubs/pub.in;
+	nvim ex$x/pubs/pub.out;
+}
+
+mkdirC(){
 	if [ "$x" -lt 10 ]; then
 			mkdir -p ex0$x/pubs;
-			nvim ex0$x/pubs/pub.in;
-			nvim ex0$x/pubs/pub.out;
+			writePubsLt10;
 			>ex0$x/ex0$x.c
 	elif [ "$x" -ge 10 ]; then
 			mkdir -p ex$x/pubs;
-			nvim ex$x/pubs/pub.in;
-			nvim ex$x/pubs/pub.out;
+			writePubsGe10;
 			> ex$x/ex$x.c;
 	fi;
-elif [ "$y" == "j" ]; then
+}
+
+mkdirJ(){
 	if [ "$x" -lt 10 ]; then
 			mkdir -p ex0$x/pubs;
-			nvim ex0$x/pubs/pub.in;
-			nvim ex0$x/pubs/pub.out;
+			writePubsLt10;
 			> ex0$x/Ex0$x.java;
 	elif [ "$x" -ge 10 ]; then
 			mkdir -p ex$x/pubs;
-			nvim ex$x/pubs/pub.in;
-			nvim ex$x/pubs/pub.out;
+			writePubsGe10;
 			> ex$x/Ex$x.java;
 	fi;
+}
+
+read -p "Digite a linguagem (c) Language ou (j)ava:" language;
+read -p "Digite o número da questão:" x;
+
+if [ "$language" == "c" ]; then
+	mkdirC;
+elif [ "$language" == "j" ]; then
+	mkdirJ;
 else
 	echo "Não encontrei o tipo do exercício";
 fi;

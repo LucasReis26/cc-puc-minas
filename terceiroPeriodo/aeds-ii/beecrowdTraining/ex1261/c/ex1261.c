a37a631a3a22dd5e52af0568974baa3ebdc50484
Lucas Reis <luctreis@gmail.com>
2025-03-24 16:05:41 -0300

    Adicionando provas práticas ao github

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(){
 	int quant_palavras,
-		quant_descricoes,
-		*values = (int *)malloc(quant_palavras * sizeof(int));
+		quant_descricoes;
 
+	scanf("%d %d",&quant_palavras, &quant_descricoes);
+
+	int *values = (int *)malloc(quant_palavras * sizeof(int));
 	char **words = (char **)malloc(quant_palavras * sizeof(char));
 	
 	for(int i = 0; i < quant_palavras; i++){
-		words[i] = (int *)malloc(255*sizeof(char));
-		scanf("%s %d",words[i], values[i]);
+		words[i] = (char *)malloc(255*sizeof(char));
+		printf("O valor do i é: %d\n",i);
+		scanf("%s",words[i]);
+		scanf("%d",&values[i]);
 		getchar();
+		printf("Palavra: %s, custa: %d\n",words[i],values[i]);
 	}
 
+	for(int i = 0; i < quant_descricoes; i++){
 
+		char *w = (char *)malloc(255*sizeof(char));
+		int descriptionValue = 0;
 
-	for(int i = 0; i < quant_palavras; i++){
-		free(words[i]);
+		do{
+			scanf("%s",w);
+			printf("Palavra escaneada: %s\n",w);
+			for(int j = 0; j < quant_palavras; j++){
+				printf("Palavra testada: %s\n",words[j]);
+				if(strcmp(w,words[j]) == 0){
+					descriptionValue += values[j];
+					printf("%s eh igual a %s\n",w,words[j]);
+					j = quant_palavras;
+				}
+			}
+
+		}while(strcmp(w,".") != 0);
+		
+		printf("%d\n",descriptionValue);
+
+		free(w);
+
 	}
+
+	// for(int i = 0; i < quant_palavras; i++){
+	// 	free(words[i]);
+	// }
 	free(words);
 	free(values);
 
---
+++ terceiroPeriodo/aeds-ii/provaPratica/ex01/java/Ex01.java
@@ -0,0 +1,94 @@
+package ex01.java;
+
+import java.util.Scanner;
+
+class Paises{
+	public String name;
+	public Integer ouros;
+	public Integer pratas;
+	public Integer bronzes;
+
+	public Paises(String name, Integer ouros, Integer pratas, Integer bronzes){
+		this.name = name;
+		this.ouros = ouros;
+		this.pratas = pratas;
+		this.bronzes = bronzes;
+	}
+
+	public void print(){
+		System.out.println(this.name + " " + this.ouros + " " + this.pratas + " " + this.bronzes);
+	}
+}
+
+public class Ex01{
+
+	public static void ordenador(Paises[] p){
+		Integer len = p.length;
+
+		for(int i = 0; i < len - 1; i++){
+			boolean swapped = false;
+			for(int j = 0; j < len - i - 1; j++){
+
+				Boolean v1 = (p[j].ouros < p[j + 1].ouros),
+						v2 = (p[j].pratas < p[j + 1].pratas),
+						v3 = (p[j].bronzes < p[j + 1].bronzes),
+						v4 = (p[j].ouros == p[j + 1].ouros),
+						v5 = (p[j].pratas == p[j + 1].pratas),
+						v6 = (p[j].bronzes == p[j + 1].bronzes),
+						v7 = (p[j].name.compareTo(p[j+1].name) == -1);
+
+				if(v1){
+					swapped = true;
+					Paises tmp = p[j];
+					p[j] = p[j+1];
+					p[j + 1] = tmp;
+				}else if(v2){
+					swapped = true;
+					Paises tmp = p[j];
+					p[j] = p[j+1];
+					p[j + 1] = tmp;
+				}else if(v3){
+					swapped = true;
+					Paises tmp = p[j];
+					p[j] = p[j+1];
+					p[j + 1] = tmp;
+				}else if(v4 && v5 && v6 && v7){
+					swapped = true;
+					Paises tmp = p[j];
+					p[j] = p[j+1];
+					p[j + 1] = tmp;
+				}
+			}
+			if(!swapped){
+				i = len;
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		Integer numPaises = sc.nextInt();
+
+		Paises[] paises = new Paises[numPaises];
+
+		for(int i = 0; i < numPaises; i++){
+
+			String name = sc.next();
+
+			Integer ouros = sc.nextInt(),
+					pratas = sc.nextInt(),
+					bronzes = sc.nextInt();
+
+			paises[i] = new Paises(name,ouros,pratas,bronzes);
+		}
+
+		ordenador(paises);
+
+		for(int i = 0; i < numPaises; i++){
+			paises[i].print();
+		}
+
+		sc.close();
+	}
+}

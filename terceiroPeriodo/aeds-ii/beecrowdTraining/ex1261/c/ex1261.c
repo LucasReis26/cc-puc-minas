db27bd7d5a75ee0f45cf906c54487ee84e7408ac
Lucas Reis <luctreis@gmail.com>
2025-03-24 09:43:37 -0300

    Atualizando variáveis

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
@@ -4,9 +4,10 @@
 int main(){
 	int quant_palavras,
 		quant_descricoes,
-		*words = (int *)malloc(quant_palavras * sizeof(int)),
 		*values = (int *)malloc(quant_palavras * sizeof(int));
 
+	char **words = (char **)malloc(quant_palavras * sizeof(char));
+	
 
 	free(words);
 	free(values);

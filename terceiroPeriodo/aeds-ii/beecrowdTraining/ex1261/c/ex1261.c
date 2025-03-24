4d6a09b4fdba4887d2fab099c69706f5f6bfd08b
Lucas Reis <luctreis@gmail.com>
2025-03-24 09:36:21 -0300

    variáveis criadas

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1261/c/ex1261.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int main(){
+	int quant_palavras,
+		quant_descricoes,
+		*words = (int *)malloc(quant_palavras * sizeof(int)),
+		*values = (int *)malloc(quant_palavras * sizeof(int));
 
+
+	free(words);
+	free(values);
 
 	return 0;
 }

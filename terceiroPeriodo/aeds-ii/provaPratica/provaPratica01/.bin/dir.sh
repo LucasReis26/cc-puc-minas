70caeec5280cc7156db988c6d05149d4e18a94a2
Lucas Reis <luctreis@gmail.com>
2025-03-25 11:18:55 -0300

    Ajustando MakeFile para provas práticas

--- terceiroPeriodo/aeds-ii/provaPratica/provaPratica01/.bin/dir.sh
+++ terceiroPeriodo/aeds-ii/provaPratica/provaPratica01/.bin/dir.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
+my_dir=""
+
 writePubsLt10(){
-	nvim ex0$x/pubs/pub.in;
-	nvim ex0$x/pubs/pub.out;
+	nvim "ex0$x/pubs/pub.in"
+	nvim "ex0$x/pubs/pub.out"
 }
 
 writePubsGe10(){
-	nvim ex$x/pubs/pub.in;
-	nvim ex$x/pubs/pub.out;
+	nvim "ex$x/pubs/pub.in"
+	nvim "ex$x/pubs/pub.out"
 }
 
 mkExFilesLt10(){
-	>ex0$x/c/ex0$x.c
-	>ex0$x/cpp/ex0$x.cpp
-	>ex0$x/java/Ex0$x.java
+	> "ex0$x/$my_dir/ex0$x.$my_dir"
 }
 
 mkExFilesGe10(){
-	>ex$x/c/ex$x.c
-	>ex$x/cpp/ex$x.cpp
-	>ex$x/java/Ex$x.java
+	> "ex$x/$my_dir/ex$x.$my_dir"
 }
 
 mkdir_ex(){
+	if [ "$op" -eq 1 ]
+	then
+		my_dir="c"
+	elif [ "$op" -eq 2 ]
+	then
+		my_dir="java"
+	fi
+
 	if [ "$x" -lt 10 ]
 	then
-			mkdir -p ex0$x/pubs
-			mkdir ex0$x/c
-			mkdir ex0$x/cpp
-			mkdir ex0$x/java
+			mkdir -p "ex0$x/pubs"
+			mkdir "ex0$x/$my_dir/"
 			writePubsLt10
 			mkExFilesLt10
 	elif [ "$x" -ge 10 ] 
 	then
-			mkdir -p ex$x/pubs
-			mkdir ex$x/c
-			mkdir ex$x/cpp
-			mkdir ex$x/java
+			mkdir -p "ex$x/pubs"
+			mkdir "ex$x/$my_dir/"
 			writePubsGe10
 			mkExFilesGe10
 	fi
 }
 
-read -p "Digite o número da questão:" x;
+read -p "Digite o número da questão:" x
+echo "Escolha a linguagem que você deseja fazer a questão:"
+echo " 1 - C;"
+echo " 2 - Java;"
+read -p "Sua opção: " op
 
-mkdir_ex;
-
+mkdir_ex
 

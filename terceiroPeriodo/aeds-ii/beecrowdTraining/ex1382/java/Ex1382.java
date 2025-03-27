6d9c9448273d0811fc2257cc53223ca85cb4844e
Lucas Reis <luctreis@gmail.com>
2025-03-27 08:08:56 -0300

    Primeira versão 1382

---
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1382/java/Ex1382.java
@@ -0,0 +1,61 @@
+package ex1382.java;
+
+import java.util.Scanner;
+
+public class Ex1382{
+
+	public static Integer ordena(Integer[] arr){
+
+		Integer len = arr.length,
+				count = 0;
+
+		for(int i = 0; i < len - 1; i++){
+
+			Integer menor = i;
+
+			for(int j = i + 1; j < len; j++){
+
+				if(arr[menor] > arr[j]){
+
+					menor = j;
+				}
+			}
+
+			Integer tmp = arr[i];
+			arr[i] = arr[menor];
+			arr[menor] = tmp;
+			count++;
+		}
+
+		return count;
+
+	}
+
+	public static void preenche(Integer[] arr, Scanner sc){
+		Integer len = arr.length;
+
+		for(int i = 0; i < len; i++){
+
+			arr[i] = sc.nextInt();
+		}
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		Integer instances = sc.nextInt();
+
+		for(int i = 0; i < instances; i++){
+
+			Integer range = sc.nextInt();
+
+			Integer[] array = new Integer[range];
+
+			preenche(array,sc);
+
+			System.out.println(ordena(array));
+		}
+
+		sc.close();
+	}
+}

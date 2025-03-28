347c45bbc98f59ea1771e70bcfcdd14806f658a2
Lucas Reis <luctreis@gmail.com>
2025-03-28 20:49:56 -0300

    Renas são lidas agora

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/cpp/ex1766.cpp
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/cpp/ex1766.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <iomanip>
 
-class RENAS{
+class Rena{
+
 	public:
 		std::string nome;
 		int peso;
 		int idade;
 		float altura;
-}
+
+	Rena(){
+		nome = "Sem Nome";
+		peso = 0;
+		idade = 0;
+		altura = 0.0;
+	}
+
+	Rena(std::string nome, int peso, int idade, float altura){
+		this->nome = nome;
+		this->peso = peso;
+		this->idade = idade;
+		this->altura = altura;
+	}
+};
 
 int main(){
 
@@ -21,6 +36,22 @@ int main(){
 			renasTreno;
 
 		std::cin >> totalRenas >> renasTreno;
+
+		Rena *renas = new Rena[totalRenas];
+
+		for(int j = 0; j < totalRenas; j++){
+
+			std::string nome;
+			int peso;
+			int idade;
+			float altura;
+
+			std::cin >> nome >> peso >> idade >> altura;
+
+			renas[0] = Rena(nome,peso,idade,altura);
+		}
+
+		delete[] renas;
 
 	}
 

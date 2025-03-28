91c3e8dd9dd9f2611d7ad882d34a77a31e0a4d4d
Lucas Reis <luctreis@gmail.com>
2025-03-28 20:33:52 -0300

    lendo quantidade e criando classe rena

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/cpp/ex1766.cpp
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/cpp/ex1766.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <iomanip>
 
+class RENAS{
+	public:
+		std::string nome;
+		int peso;
+		int idade;
+		float altura;
+}
+
 int main(){
 
 	int numCases;
+
+	std::cin >> numCases;
+
+	for(int i = 0; i < numCases; i++){
+
+		int totalRenas,
+			renasTreno;
+
+		std::cin >> totalRenas >> renasTreno;
+
+	}
 
 	return 0;
 }

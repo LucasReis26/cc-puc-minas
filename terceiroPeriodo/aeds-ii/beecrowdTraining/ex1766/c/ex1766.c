108a3c5eb2f669c3d4c7d660684648766690e72c
Lucas Reis <luctreis@gmail.com>
2025-03-27 22:38:58 -0300

    definindo renas e iniciando leitura

--- terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/c/ex1766.c
+++ terceiroPeriodo/aeds-ii/beecrowdTraining/ex1766/c/ex1766.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+
+	char nome[255];
+	int peso;
+	int idade;
+	float altura;
+
+} RENA;
+
+void leRenas(rena *x){
+	scanf("%s %d %d %f",x.nome,&x.peso,&x.idade,&x.altura);
+}
+
 int main(){
 
 	int numCases;
@@ -10,6 +23,20 @@ int main(){
 	for(int i = 0; i < numCases; i++){
 
 		printf("CENARIO {%d}\n", (i + 1));
+
+		int totalRenas,
+			renasTreno;
+
+		scanf("%d %d",&totalRenas, &renasTreno);
+
+		RENA *renas = (RENA *)malloc(totalRenas*sizeof(RENA));
+
+		for(int j = 0; j < totalRenas; j++){
+
+			leRenas(&renas[j]);
+		}
+
+		free(renas);
 	}
 
 	return 0;

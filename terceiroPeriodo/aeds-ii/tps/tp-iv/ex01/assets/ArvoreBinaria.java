778a4bd1c47ed89779e0ca6d2949638daa1c7abb
Lucas Reis <luctreis@gmail.com>
2025-06-22 22:44:28 -0300

    wip: ex01 tp-iv

--- terceiroPeriodo/aeds-ii/tps/tp-iv/ex01/assets/ArvoreBinaria.java
+++ terceiroPeriodo/aeds-ii/tps/tp-iv/ex01/assets/ArvoreBinaria.java
@@ -3,68 +3,89 @@ package ex01.assets;
 import ex01.assets.Show;
 
 public class ArvoreBinaria{
-	private Show[] array;
-	private int tam;
+	private No raiz;
 
-	public Lista(){
-		array = new Show[1368];
-		tam = 0;
+	public ArvoreBinaria(){
+		raiz = null;
 	}
 
-	public void inserirInicio(Show show){
-		for(int i = tam; i > 0; i--){
-			array[i] = array[i - 1];
+	private boolean pesquisar(int x, No i){
+		boolean resp;
+
+		if(i == null){
+			resp = false;
+		}else if(x == i.getElemento()){
+			resp = true;
+		}else if(x < i.elemento){
+			resp = pesquisar(x, i.getEsq());
+		}else{
+			resp = pesquisar(x,i.getDir());
 		}
-		array[0] = show;
-		tam++;
+
+		return resp;
 	}
 
-	public void inserir(Show show, int posicao) {
-		for(int i = tam; i > posicao; i--){
-			array[i] = array[i - 1];
+	public boolean pesquisar(int x){
+		return pesquisar(x,raiz);
+	}
+
+	private void caminharCentral(No i){
+		if(i != null){
+			caminharCentral(i.getEsq());
+			System.out.println(i.getElemento() + " ");
+			caminharCentral(i.getDir());
 		}
-		array[posicao] = show;
-		tam++;
 	}
 
-	public void inserirFim(Show show){
-		array[tam++] = show;
-	}
-
-	public Show removerInicio(){
-		Show tmp = array[0];
-		for(int i = 0; i < tam - 1; i++){
-			array[i] = array[i + 1];
+	private void caminharPre(No i){
+		if(i != null){
+			System.out.println(i.getElemento() + " ");
+			caminharPre(i.getEsq());
+			caminharPre(i.getDir());
 		}
-		tam--;
-		return tmp;
 	}
 
-	public Show remover(int posicao) {
-		// mostrar();
-		Show tmp = array[posicao];
-		for(int i = posicao; i < tam - 1; i++){
-			array[i] = array[i + 1];
+	private void caminharPos(No i){
+		if(i != null){
+			caminharPos(i.getEsq());
+			caminharPos(i.getDir());
+			System.out.println(i.getElemento() + " ");
 		}
-		tam--;
-		return tmp;
 	}
 
-	public Show removerFim(){
-		return array[--tam];
-	}
-
-	public void mostrar(){
+	public void caminharCentral(){
 		System.out.print("[ ");
-		for(int i = 0; i < tam; i++){
-			System.out.print(array[i].getShow_id() + " ");
-		}
+		caminharCentral(raiz);
 		System.out.println("]");
 	}
 
-	public void mostraRestantes(){
-		for(int i = 0; i < tam; i++){
-			array[i].imprimir();
+	public void caminharPre(){
+		System.out.print("[ ");
+		caminharPre(raiz);
+		System.out.println("]");
+	}
+
+	public void caminharPos(){
+		System.out.print("[ ");
+		caminharPos(raiz);
+		System.out.println("]");
+	}
+
+	private void inserir(int x, No i) throws Exception{
+		if(i == null){
+			i = new No(x);
+		}else if(x < i.getElemento()){
+			i.setEsq(inserir(x,i.getEsq()));
+		}else if(x > i.getElemento()){
+			i.setDir(inserir(x,i.getDir()));
+		}else{
+			throw new Exception("Erro ao inserir!");
 		}
+
+		return i;
+	}
+
+	public void inserir(int x){
+		raiz = inserir(i,raiz);
 	}
 }
